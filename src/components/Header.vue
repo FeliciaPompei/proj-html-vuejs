93765e5a36ba628e2dea89c5bc2527e4f44c49b9
FeliciaPompei <f.pompei@hotmail.com>
2022-04-07 12:18:01 +0200

    start of the project, started work on header

---
+++ src/components/Header.vue
@@ -0,0 +1,60 @@
+<template>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Logo</a>
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item text-uppercase" 
+                    v-for="(item, index) in navItems" 
+                    :key="index">
+                    <a class="nav-link active" :href="item.link">{{item.text}} </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'HeaderIndex',
+    data(){
+        return{
+            navItems : [
+                {
+                    text: 'home',
+                    link: '#'
+                },
+                {
+                    text: 'courses',
+                    link: '#'
+                },
+                {
+                    text: 'instructors',
+                    link: '#'
+                },
+                {
+                    text: 'events',
+                    link: '#'
+                },
+                {
+                    text: 'elements',
+                    link: '#'
+                },
+                {
+                    text: 'symbols',
+                    link: '#'
+                },
+                
+            ] 
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+header{
+    position:fixed;
+    top:0;
+}
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,18 @@
+<template>
+    <main>
+        <h2 class="text-success">Main</h2>
+    </main>
+</template>
+
+<script>
+export default {
+    name: 'MainIndex'
+}
+</script>
+
+<style lang="scss" scoped>
+main{
+    height: 150vh;
+    background-color: palevioletred;
+}
+</style>
